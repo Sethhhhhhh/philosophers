0f2f986cafd66c52ffbe4b2204257daf66959d1f
Sethhhhhhh <yohannvvt@gmail.com>
2021-07-04 21:55:25 +0200

    clear to implement death event.

--- srcs/tasks.c
+++ srcs/tasks.c
@@ -13,11 +13,11 @@ static void	eat(t_sys *sys, size_t id)
 	sys->p[id].eat_time = get_time();
 	msg(sys, id, TYPE_EAT);
 	usleep(sys->time_to_eat * 1000);
+	sys->p[id].is_eating = 0;
 }
 
 static void	put_fork(t_sys *sys, size_t id)
 {
-	sys->p[id].is_eating = 0;
 	pthread_mutex_unlock(&(sys->forks[id]));
 	pthread_mutex_unlock(&(sys->forks[(id + 1) % sys->philo_amount]));
 	msg(sys, id, TYPE_SLEEP);
@@ -30,16 +30,15 @@ void	*tasks(void *sys_v)
 	size_t  id;
 
 	sys = (t_sys *)sys_v;
-
 	pthread_mutex_lock(&(sys->lock));
 	sys->id++;
 	id = sys->id;
 	pthread_mutex_unlock(&(sys->lock));
-
 	while (1)
 	{
 		take_fork(sys, id);
 		eat(sys, id);
 		put_fork(sys, id);
+		msg(sys, id, TYPE_THINK);
 	}
 }
